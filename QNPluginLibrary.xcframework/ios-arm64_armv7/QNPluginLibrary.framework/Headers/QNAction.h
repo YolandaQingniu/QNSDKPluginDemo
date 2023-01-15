30010e1b122a4d814ce2bfaf4c9247d41273b846
苏蒙 <535894711@qq.com>
2023-01-15 13:39:31 +0800

    优化家用秤组件功能

---
+++ QNPluginLibrary.xcframework/ios-arm64_armv7/QNPluginLibrary.framework/Headers/QNAction.h
@@ -0,0 +1,16 @@
+//
+//  QNAction.h
+//  QNPlugin
+//
+//  Created by sumeng on 2022/6/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QNAction : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
